086ceb3f23e3b53e2333c5c2f0d282dfcd43f9b4
Harish <114534778+harishsg993010@users.noreply.github.com>
2022-12-09 06:07:48 +0530

    Update Windows_scanner.py

--- Windows_scanner.py
+++ Windows_scanner.py
@@ -1,17 +1,12 @@
 import pefile
 
 def main():
-  # Parse command-line arguments
+
   if len(sys.argv) != 2:
     print("Usage: python3 scan.py /path/to/exe")
     return
-
   exe_path = sys.argv[1]
-
-  # Parse the PE file
   pe = pefile.PE(exe_path)
-
-  # Check for common vulnerabilities in the PE file
   if pe.OPTIONAL_HEADER.IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE:
     print("EXE uses dynamic base")
   if pe.OPTIONAL_HEADER.IMAGE_DLLCHARACTERISTICS_NX_COMPAT:
